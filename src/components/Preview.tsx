9dd6c5dc7849ba28dcbd887c4c6c98f1c9e95e72
“salahwahsh12” <salahwahsh12@gmail.com>
2023-12-18 13:10:44 +0200

    update the preview layout

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import "../App.css";
+
 interface PreviewProps {
-  capturedImage: any;
+  capturedImage: object | any;
   setShowPreview: (open: boolean) => void;
 }
 
@@ -22,7 +23,6 @@ const Preview = ({ capturedImage, setShowPreview }: PreviewProps) => {
     img.src = capturedImage;
 
     return () => {
-      // Clean up the image object if the component is unmounted
       img.onload = null;
       img.onerror = null;
     };
@@ -33,11 +33,46 @@ const Preview = ({ capturedImage, setShowPreview }: PreviewProps) => {
     anchor.href = capturedImage;
     anchor.download = "captured_photo.jpg";
     anchor.click();
-    URL.revokeObjectURL(capturedImage);
-
-    //print the image not the window
-    // window.print();
+    // URL.revokeObjectURL(capturedImage);
+    printImage(capturedImage);
   };
+
+  const printImage = (capturedImage: string): void => {
+    const printContent = document.createElement("div");
+    printContent.style.textAlign = "center";
+
+    const printImage = new Image();
+    printImage.src = capturedImage;
+    printImage.style.maxWidth = "100%";
+    printImage.style.maxHeight = "100vh";
+    printContent.appendChild(printImage);
+
+    const printWindow = window.open("", "_blank");
+    if (printWindow) {
+      printWindow.document.write(`
+        <html>
+          <head>
+            <title>Print Image</title>
+          </head>
+          <body style="margin: 0;">
+            ${printContent.outerHTML}
+          </body>
+        </html>
+      `);
+
+      // Wait for the image to load before printing
+      printImage.onload = () => {
+        printWindow.print();
+        printWindow.onafterprint = () => {
+          // Close the new window or iframe after a short delay
+          window.setTimeout(() => {
+            printWindow.close();
+          }, 100);
+        };
+      };
+    }
+  };
+
   const handleRetakeClick = () => {
     setImageSrc(null);
     setShowPreview(false);
@@ -45,27 +80,35 @@ const Preview = ({ capturedImage, setShowPreview }: PreviewProps) => {
 
   return (
     <div className="preview">
-      <div className=" ">
-        {loading ? (
-          <p>Loading...</p>
-        ) : (
-          <img src={imageSrc || ""} alt="Captured Preview" width="200px" />
-        )}
-
-        <button
-          className="bg-blue-500 hover:bg-blue-700
-       text-white font-bold py-2 px-4 rounded"
-          onClick={handlePrintClick}
-        >
-          Print
-        </button>
+      <div className="button-container top-0 left-0 w-full flex justify-between p-4 absolute">
         <button
           onClick={handleRetakeClick}
-          className="bg-red-500 hover:bg-red-700
-       text-white font-bold py-2 px-4 rounded"
+          className="bg-red-500 hover:bg-red-700 text-white font-bold text-sm md:text-lg lg:text-xl py-2 px-4 md:w-40 h-20 rounded-full"
         >
           Retake
         </button>
+        <div className="flex space-x-4">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold text-sm md:text-lg lg:text-xl py-2 px-4 md:w-40 h-20 rounded-full"
+            onClick={handlePrintClick}
+          >
+            Print
+          </button>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold text-sm md:text-lg lg:text-xl py-2 px-4 md:w-40 h-20 rounded-full">
+            Record Audio
+          </button>
+        </div>
+      </div>
+      <div className="preview-frame mt-8">
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <img
+            src={imageSrc || ""}
+            alt="Captured Preview"
+            className="preview-image"
+          />
+        )}
       </div>
     </div>
   );
